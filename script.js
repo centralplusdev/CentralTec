0b26e3762327cb41071357af810dcfd25c33346c
felipenevesdamata-boop <felipe.neves.da.mata@gmail.com>
2026-08-22 14:34:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function abrirTutorial(categoria) {
-    alert("Você escolheu a categoria: " + categoria + " 🚀");
+    if (categoria === "Servidores") {
+        window.location.href = "servidores.html";
+    }
+
+    if (categoria === "Discord") {
+        alert("Os tutoriais de Discord serão adicionados em breve!");
+    }
+
+    if (categoria === "Web") {
+        alert("Os tutoriais de Web serão adicionados em breve!");
+    }
 }
