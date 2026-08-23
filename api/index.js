c846d3422b67653c91f22c03bd066c02ddab5fef
CentralTech <lipezingg04@gmail.com>
2026-08-22 22:41:59 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        sucesso: true,
+        mensagem: "Backend da Central+ funcionando!"
+    });
+});
+
+app.get("/api/teste", (req, res) => {
+    res.json({
+        sucesso: true,
+        mensagem: "API funcionando corretamente!"
+    });
+});
+
+module.exports = app;
